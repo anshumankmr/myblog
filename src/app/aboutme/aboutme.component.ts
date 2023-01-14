5ec3d8a1e021dec9d79c9a193afe7686731472d3
Anshuman Kumar <anshumankmr80@gmail.com>
2023-01-14 15:28:49 +0530

    update title

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutme.component.css'],
 })
 export class AboutmeComponent implements OnInit {
+  imageCaption =
+    'A Picture I took Near Baba Ka Mandir near Natu La Pass' +
+    '. You can view the rest of my Pictures ';
   constructor() {}
 
   ngOnInit(): void {}

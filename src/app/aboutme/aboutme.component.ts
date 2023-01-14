4e8c5b23298d8d4d8e7544e0edf053722698dda5
Anshuman Kumar <anshumankmr80@gmail.com>
2023-01-14 17:48:28 +0530

    Fix CSS Rules, Add Gradient Color

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -9,6 +9,9 @@ export class AboutmeComponent implements OnInit {
   imageCaption =
     'A Picture I took Near Baba Ka Mandir near Natu La Pass' +
     '. You can view the rest of my Pictures ';
+  introductionText =
+    'Hi, I am Anshuman Kumar. I am Software Developer. My Passions Include Software Design, Cooking , Cycling';
+  introductionTitle = 'Welcome to my Blog';
   constructor() {}
 
   ngOnInit(): void {}

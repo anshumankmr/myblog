7e8d577e2e46c35eb941894d772134a9890afba9
Anshuman Kumar <anshumankmr80@gmail.com>
2023-06-17 16:07:24 +0530

    Update aboutme.component.ts

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -9,7 +9,7 @@ export class AboutmeComponent implements OnInit {
   myNameIs = 'Hi, my name is <span>Anshuman Kumar</span>';
   aboutMe = `Currently, I'm working for <a href="https://quantiphi.com"> Quantiphi</a> as
   Software Developer. I <3 Jam Stack. This site was built with Angular and Scully.I am proficient with NodeJS, Angular, 
-  Python, React, CSS.In my spare time, I love cycling and participating in marathons. For example, in 2022, I participated in the Vedanta Delhi Half Marathon. I am also an amateur photography enthusiast.You can view some of my clicks here <a href="https://unsplash.com/@anshumankmr97" class="anchor-tag">here</a>.`;
+  Python, React, CSS.In my spare time, I love cycling and participating in marathons. For example, in 2022, I participated in the Vedanta Delhi Half Marathon. I am also an amateur photography enthusiast.You can view some of my clicks here <a href="https://unsplash.com/@anshumankmr97" class="anchor-tag">here</a>. <br/>Proud to be featured on <a href="https://1mb.club/" class="anchor-tag">1mb Club</a>.`;
   constructor() {}
 
   ngOnInit(): void {}

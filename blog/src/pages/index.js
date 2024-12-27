6e07e5362b0ae66c8042241dc7ff3618bd931841
Anshuman Kumar <anshumankmr80@gmail.com>
2024-12-27 15:47:04 +0530

    Fix API Calling Logic by Adding API Call to index.js

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,66 +1,128 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import axios from 'axios'
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const BlogIndex = ({ pageContext, location }) => {
-  const { siteTitle, posts } = pageContext;
+const BlogPostTemplate = ({ location }) => {
+  const [post, setPost] = React.useState(null)
+  const [loading, setLoading] = React.useState(true)
+  const [error, setError] = React.useState(null)
+  const siteTitle = "Your Site Title"
 
-  if (!posts || posts.length === 0) {
+  React.useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        // Extract the date and title from the URL
+        const pathParts = location.pathname.split('/')
+        const date = pathParts[2] // Assuming URL structure is /article/date/title
+        const slug = pathParts[3]
+
+        const response = await axios.get('https://glass-approach-204914.uc.r.appspot.com/api/blogs')
+        const blogs = response.data.data
+
+        // Find the matching blog post
+        const matchedPost = blogs.find(blog => {
+          const blogDate = blog.attributes.date
+          const blogSlug = generateSlug(blog.attributes.Title)
+          return blogDate === date && blogSlug === slug
+        })
+
+        if (!matchedPost) {
+          throw new Error('Blog post not found')
+        }
+
+        setPost(matchedPost.attributes)
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPost()
+  }, [location.pathname])
+
+  if (loading) {
     return (
-      <Layout location={location} title={siteTitle || `Title`}>
-        <Bio />
-        <p>No blog posts found. Please add some posts to your data source.</p>
+      <Layout location={location} title={siteTitle}>
+        <p>Loading blog post...</p>
+      </Layout>
+    )
+  }
+
+  if (error) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <p>Error: {error}</p>
+        <Link to="/">Go back to home</Link>
+      </Layout>
+    )
+  }
+
+  if (!post) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <p>Blog post not found</p>
+        <Link to="/">Go back to home</Link>
       </Layout>
     )
   }
 
   return (
-    <Layout location={location} title={siteTitle || `Title`}>
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const { slug, title, date, description, excerpt } = post;
-
-          return (
-            <li key={slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: description || excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+    <Layout location={location} title={siteTitle}>
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h1 itemProp="headline">{post.Title}</h1>
+          <p>{post.date}</p>
+        </header>
+        <section
+          dangerouslySetInnerHTML={{ __html: post.Content }}
+          itemProp="articleBody"
+        />
+        <hr />
+        <footer>
+          <Bio />
+        </footer>
+      </article>
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            <Link to="/" rel="prev">
+              ← Back to all posts
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </Layout>
   )
 }
 
-export default BlogIndex
+// Function to generate a URL-friendly slug (same as in index.js)
+function generateSlug(title) {
+  return title
+    .toLowerCase()
+    .replace(/[\s\(\)]+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="All posts" />
+export default BlogPostTemplate
+
+export const Head = () => <Seo title="Blog Post" />
